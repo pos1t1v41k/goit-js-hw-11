7ecd965c918b20cd85d965725daaa79b06cf1cb3
Yehor Kikas <ekikas13@gmail.com>
2022-10-26 20:19:32 +0300

    fix getImage

--- src/js/getImages.js
+++ src/js/getImages.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 export async function getImages(keyWord, nrPage) {
    try {
       const response = await axios.get(
-         `https://pixabay.com/api/?key=30856606-19b329139abde22b1a8069758=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true&page=${nrPage}&per_page=40`
+         `https://pixabay.com/api/?key=30856606-19b329139abde22b1a8069758&q=${keyWord}&image_type=photo&orientation=horizontal&safesearch=true&page=${nrPage}&per_page=40`
       );
       return response;
    } catch (error) {
